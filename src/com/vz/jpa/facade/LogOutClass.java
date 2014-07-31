6d1f2be1fbae1f87bbc445d2994dcca48228744b
Vagabone <Vagabone10@bigmir.net>
2014-07-31 13:18:49 +0300

    Логаут, + библиотеки к рассылке почты

---
+++ src/com/vz/jpa/facade/LogOutClass.java
@@ -0,0 +1,22 @@
+package com.vz.jpa.facade;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.NewCookie;
+import javax.ws.rs.core.Response;
+
+@Path("/logout")
+public class LogOutClass {
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	static public Response logOutByDeleteCookies() {
+		return Response
+				.ok()
+				.cookie(new NewCookie("client id", null),
+						new NewCookie("client email", null),
+						new NewCookie("role", null)).build();
+	}
+}
