e716e0af188ed5236360fa13f871babdff7ec6de
Sanekkit <akitaygora@mail.ua>
2014-07-31 13:01:36 +0300

    Cha

--- src/com/vz/jpa/dao/ClientDaoImpl.java
+++ src/com/vz/jpa/dao/ClientDaoImpl.java
@@ -16,7 +16,8 @@ public class ClientDaoImpl implements ClientDao {
     @Override
     public List<Client> selectAll(){
         EntityManager entityManager = entityManagerFactory.createEntityManager();
-        Query query = entityManager.createQuery("SELECT c FROM Client c");
+        Query query = entityManager.createQuery("SELECT Client_id, Name, Surname, Birthday, Sex, " +
+                "Phone, E_mail, Password, City_id, Passport, INN, Visa, Comment, Country_id FROM Client ");
         List<Client> clients = query.getResultList();
         entityManager.close();
         return clients;
@@ -25,30 +26,14 @@ public class ClientDaoImpl implements ClientDao {
     @Override
     public Client selectById(int id){
         EntityManager entityManager = entityManagerFactory.createEntityManager();
-        Query query = entityManager.createQuery("SELECT c FROM Client c WHERE c.clientId = :c_id");
+        Query query = entityManager.createQuery("SELECT  Client_id, Name, Surname, Birthday, Sex,Phone, " +
+                "E_mail, Password, City_id, Passport, INN, Visa, Comment, Country_id FROM Client WHERE City_id = :c_id");
         query.setParameter("c_id",id);
         Client client = (Client) query.getSingleResult();
         entityManager.close();
         return client;
     }
     
-    @Override
-    public boolean clientExistCheck(String email, String password) {
-  	  EntityManager entityManager = entityManagerFactory.createEntityManager();
-  	  Client client;
-  	  try {
-  		  Query query = entityManager.createQuery("SELECT c FROM Client c WHERE "
-  				  + "c.eMail = :email AND c.password = :pass ");
-  		  query.setParameter("pass", password);
-  		  query.setParameter("email", email);
-  		  client = (Client) query.getSingleResult();
-  	  } catch (javax.persistence.NoResultException e) {
-  		  	return false;
-  	  }
-  	  	entityManager.close();
-  	  	return true;
-  	 }
-
     @Override
     public Client selectByEmailandPassword(String email, String password){
     	EntityManager entityManager = entityManagerFactory.createEntityManager();
@@ -66,6 +51,49 @@ public class ClientDaoImpl implements ClientDao {
     	return client;
     }
     
+    public boolean clientExistCheck(String email, String password) {
+    	  EntityManager entityManager = entityManagerFactory.createEntityManager();
+    	  Client client;
+    	  try {
+    	   Query query = entityManager.createQuery("SELECT c FROM Client c WHERE "
+    	       + "c.eMail = :email AND c.password = :pass ");
+    	   query.setParameter("pass", password);
+    	   query.setParameter("email", email);
+    	   client = (Client) query.getSingleResult();
+    	  } catch (javax.persistence.NoResultException e) {
+    	   return false;
+    	  }
+    	  entityManager.close();
+    	  return true;
+    	 }
+    
+    /*@Override
+    public List<Client> selectByEmailandPassword(String email, String password){
+     EntityManager entityManager = entityManagerFactory.createEntityManager();
+     Query query = entityManager.createQuery("SELECT eMail, password FROM Client WHERE "
+       + "eMail = :email AND password = :pass ");
+        query.setParameter("pass", password);
+        query.setParameter("email", email);
+     List<Object> result = query.getResultList();
+	  List<Client> user = new ArrayList<Client>();
+	  Iterator itr = result.iterator();
+	  while (itr.hasNext()) {
+		   Client temp = new Client();
+		   Object[] obj = (Object[]) itr.next();
+		   temp.seteMail(String.valueOf(obj[0]));
+		   temp.setPassword(String.valueOf(obj[1]));
+		   user.add(temp);
+	  }
+	  entityManager.close();
+	  String msg;
+	  if(user.isEmpty()){
+		  msg="This user don`t exists";
+	  }else{
+		  msg="This user exists";
+	  }
+	  return user;
+    }
+    */
     @Override
     public boolean selectByEmail(String email){
     	EntityManager entityManager = entityManagerFactory.createEntityManager();
@@ -79,6 +107,22 @@ public class ClientDaoImpl implements ClientDao {
     	} catch (javax.persistence.NoResultException e){
     		entityManager.close();
     		return false;
+    	}    	
+    }
+    
+    @Override
+    public Client selectByEmailClient (String email){
+    	EntityManager entityManager = entityManagerFactory.createEntityManager();
+      	try{
+    		Query query = entityManager.createQuery("SELECT c FROM Client c WHERE "
+    				+ "c.eMail = :email");
+    		query.setParameter("email", email);
+    		Client client = (Client) query.getSingleResult();
+    		entityManager.close();
+    		return client;
+    	} catch (javax.persistence.NoResultException e){
+    		entityManager.close();
+    		return null;
     	}    	
     }
     
@@ -99,9 +143,9 @@ public class ClientDaoImpl implements ClientDao {
         client1.setSurname(client.getSurname());
         client1.setSex(client.getSex());
         client1.setBirthday(client.getBirthday());
-        client1.setPhone(client.getPhone());
+        client1.setPhone(client.getPhone());/*
         client1.seteMail(client.geteMail());
-        client1.setPassword(client.getPassword());
+        client1.setPassword(client.getPassword());*/
         client1.setPassport(client.getPassport());
         client1.setInn(client.getInn());
         client1.setVisa(client.getVisa());
